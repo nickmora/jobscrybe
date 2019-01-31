78d2b4297b3c444786026a9755af47ee9fb5f0e5
Mac User <macuser@Mac-Users-iMac-6.local>
2019-01-31 13:13:11 -0800

    front end is talking to the back end, needs additional db stuff

--- routes/index.js
+++ routes/index.js
@@ -1,59 +1,79 @@
-const fs = require('fs');
-const path = require('path');
-const db = require("../models");
-const passport = require("../config/passport");
 
-module.exports = function(app) {
-    // Using the passport.authenticate middleware with our local strategy.
-    // If the user has valid login credentials, send them to the members page.
-    // Otherwise the user will be sent an error
-    app.post("/api/login", passport.authenticate("local"), function(req, res) {
-      // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
-      // So we're sending the user back the route to the members page because the redirect will happen on the front end
-      // They won't get this or even be able to access this page if they aren't authed
-      res.json("/members");
+// const router = require("express").Router();
+// const userController = require("../controllers/userController")
+// // const resumeRoutes = require("./api/resumeSaved");
+// const db = require("../models");
+// const passport = require("../config/passport")
+// const isAuthenticated=require("../config/middleware/isAuthenticated")
+// const api = require("./api-routes")
+
+// router.route("/", api);
+
+// router.route("/api/signup")
+//     .post(userController.create);
+
+// module.exports=router;
+
+
+// const router = require("express").Router();
+// const resumeRoutes = require("./api/resumeSaved");
+const db = require("../models");
+const passport = require("../config/passport")
+const isAuthenticated=require("../config/middleware/isAuthenticated")
+
+module.exports=function(app){
+    app.get("/", (req,res)=>{
+        res.json("hey")
+    })
+    app.post("api/login", passport.authenticate("local"), (req,res)=>{
+        res.json("/members");
     });
-  
-  
-    // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
-    // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
-    // otherwise send back an error
-    app.post("/api/signup", function(req, res) {
-      console.log(req.body);
-      db.User.create({
-        email: req.body.email,
-        password: req.body.password
-      }).then(function() {
-        res.redirect(307, "/api/login");
-      }).catch(function(err) {
-        console.log(err);
-        res.json(err);
-        // res.status(422).json(err.errors[0].message);
-      });
+    app.post("/api/signup", (req,res)=>{
+        console.log(req.body);
+        db.User.create({
+            email:req.body.email,
+            password:req.body.password
+        }).then(()=>{
+            console.log("did it!")
+            res.redirect(307, "/api/login");
+        }).catch(err=>{
+            console.log(err);
+            res.json(err);
+        })
+    })
+    app.get("/logout", (req,res)=>{
+        req.logout();
+        res.redirect("/");
     });
-  
-    // Route for logging user out
-    app.get("/logout", function(req, res) {
-      req.logout();
-      res.redirect("/");
-    });
-  
-    // Route for getting some data about our user to be used client side
-    app.get("/api/user_data", function(req, res) {
-      if (!req.user) {
-        // The user is not logged in, send back an empty object
-        res.json({});
-      }
-      else {
-        // Otherwise send back the user's email and id
-        // Sending back a password, even a hashed password, isn't a good idea
-        res.json({
-          email: req.user.email,
-          id: req.user.id,
-          interests: ["movies", "sports", "fishing"]
-        });
-      }
-    });
-  
-  };
-  
+}
+//     //html routes here
+
+//     app.get("/", function(req, res) {
+//         // If the user already has an account send them to the members page
+//         if (req.user) {
+//           res.redirect("/members");
+//         }
+//         res.sendFile(path.join(__dirname, "../public/signup.html"));
+//       });
+    
+//       app.get("/login", function(req, res) {
+//         // If the user already has an account send them to the members page
+//         if (req.user) {
+//           res.redirect("/members");
+//         }
+//         res.sendFile(path.join(__dirname, "../public/login.html"));
+//       });
+    
+//       // Here we've add our isAuthenticated middleware to this route.
+//       // If a user who is not logged in tries to access this route they will be redirected to the signup page
+//       app.get("/members", isAuthenticated, function(req, res) {
+//         res.sendFile(path.join(__dirname, "../public/members.html"));
+//       });
+    
+// }
+
+
+// // Book routes
+// // router.use("/resumeSaved", resumeRoutes);
+
+// // module.exports = router;
--- server.js
+++ server.js
@@ -1,70 +1,31 @@
 const express = require('express');
-const fs = require('fs');
-// const historyApiFallback = require('connect-history-api-fallback');
 const mongoose = require('mongoose');
-const path = require('path');
-// const webpack = require('webpack');
-// const webpackDevMiddleware = require('webpack-dev-middleware');
-// const webpackHotMiddleware = require('webpack-hot-middleware');
+const router = require("express").Router();
+const userController = require("./controllers/userController")
 
-// const config = require('../config/config');
-// const webpackConfig = require('../webpack.config');
+const port  = process.env.PORT || 3001;
 
-// const isDev = process.env.NODE_ENV !== 'production';
-const port  = process.env.PORT || 3000;
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/jobscrybe_testOne');
 
-
-// Configuration
-// ================================================================================================
-
-// Set up Mongoose
-// mongoose.connect(isDev ? config.db_dev : config.db);
-mongoose.connect('mongodb://localhost/my_database');
-mongoose.Promise = global.Promise;
+// MongoClient.connect({ useNewUrlParser: true })
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // API routes
-require('./routes')(app);
+require("./routes")(app)
 
-// if (isDev) {
-//   const compiler = webpack(webpackConfig);
+// router.route("/api/signup")
+//     .post(userController.create);
 
-//   app.use(historyApiFallback({
-//     verbose: false
-//   }));
 
-//   app.use(webpackDevMiddleware(compiler, {
-//     publicPath: webpackConfig.output.publicPath,
-//     contentBase: path.resolve(__dirname, '../client/public'),
-//     stats: {
-//       colors: true,
-//       hash: false,
-//       timings: true,
-//       chunks: false,
-//       chunkModules: false,
-//       modules: false
-//     }
-//   }));
 
-//   app.use(webpackHotMiddleware(compiler));
-//   app.use(express.static(path.resolve(__dirname, '../dist')));
-// } else {
-//   app.use(express.static(path.resolve(__dirname, '../dist')));
-//   app.get('*', function (req, res) {
-//     res.sendFile(path.resolve(__dirname, '../dist/index.html'));
-//     res.end();
-//   });
-// }
 
-app.listen(port, '0.0.0.0', (err) => {
+app.listen(port, (err) => {
   if (err) {
     console.log(err);
   }
-
-  console.info('>>> 🌎 Open http://0.0.0.0:%s/ in your browser.', port);
+  
+  console.log(`Now listening on PORT ${port}, check it out`)
 });
-
-module.exports = app;
