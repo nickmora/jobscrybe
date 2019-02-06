eb9f4e1cac73e7840f23ce35032d3ace4c55918a
ashay <albertshay888@gmail.com>
2019-02-06 14:57:04 -0800

    github search result fixed

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -5,7 +5,7 @@ export default {
     getJobs: function(q, loc) {
       // console.log(q);
       // console.log(loc);
-      return axios.get(`http://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?description=${q.search}&location=${q.location}`).then(data=>{console.log(data)});
+      return axios.get(`http://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?description=${q.search}&location=${q.location}`);
     },
     // Gets all saved jobs
     getSavedJobs: function() {
